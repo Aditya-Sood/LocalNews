70ef04e6682b54f6fd09839536094e1c2d56c48b
Aditya-Sood <sood.aditya.08@gmail.com>
2019-01-31 22:13:13 +0530

    Addition: Implicit intent to open news articles

--- app/src/main/java/com/example/sood/localnews/MainActivity.java
+++ app/src/main/java/com/example/sood/localnews/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.sood.localnews;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +29,33 @@ public class MainActivity extends AppCompatActivity {
         newsApiTextView.setMovementMethod(LinkMovementMethod.getInstance());
         newsApiTextView.setText(android.text.Html.fromHtml("<a href='https://newsapi.org/'> Powered by News API </a>"));
 
-        ListView newsList = findViewById(R.id.news_list_view);
+        final ListView newsList = findViewById(R.id.news_list_view);
 
         NewsApiRequest.setNewsArticlesList(getApplicationContext(), newsList, this);
+
+        newsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                NewsItem selectedItem = (NewsItem) newsList.getItemAtPosition(position);
+
+                /*
+                * Using an implicit intent instead of a webview to avoid redundancy
+                * and keep app size small
+                * */
+                Uri webpage = Uri.parse(selectedItem.getUrl());
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                PackageManager packageManager = getPackageManager();
+                List<ResolveInfo> activities = packageManager.queryIntentActivities(webIntent, 0);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe) {
+                    startActivity(webIntent);
+                }
+
+            }
+        });
 
 /*
         for(int i = 0; i < 10; i++)
