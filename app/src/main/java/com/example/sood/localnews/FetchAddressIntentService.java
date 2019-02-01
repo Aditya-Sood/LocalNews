6899718c0d693f102b2fd4a64ba14a0282a28b98
Aditya-Sood <sood.aditya.08@gmail.com>
2019-02-02 03:37:36 +0530

    Add supporting files for reverse geocoding

---
+++ app/src/main/java/com/example/sood/localnews/FetchAddressIntentService.java
@@ -0,0 +1,94 @@
+package com.example.sood.localnews;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import static com.example.sood.localnews.MainActivity.TAG;
+
+/**
+ * Created by sood on 2/2/19.
+ */
+
+public class FetchAddressIntentService extends IntentService {
+
+    protected ResultReceiver mReceiver;
+
+    public FetchAddressIntentService() {
+        super("FetchAddressIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        mReceiver = intent.getParcelableExtra(Constants.RECEIVER);
+
+        if (intent == null) {
+            return;
+        }
+        String errorMessage = "";
+
+        // Get the location passed to this service through an extra.
+        Location location = intent.getParcelableExtra(Constants.LOCATION_DATA_EXTRA);
+
+        // ...
+
+        List<Address> addresses = null;
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    // In this sample, get just a single address.
+                    1);
+        } catch (IOException ioException) {
+            // Catch network or other I/O problems.
+            errorMessage = "service_not_available";
+            Log.e(TAG, errorMessage, ioException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            // Catch invalid latitude or longitude values.
+            errorMessage = "invalid_lat_long_used";
+            Log.e(TAG, errorMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), illegalArgumentException);
+        }
+
+        // Handle case where no address was found.
+        if (addresses == null || addresses.size()  == 0) {
+            if (errorMessage.isEmpty()) {
+                errorMessage = "no_address_found";
+                Log.e(TAG, errorMessage);
+            }
+            deliverResultToReceiver(Constants.FAILURE_RESULT, errorMessage);
+        } else {
+            Address address = addresses.get(0);
+
+            Log.i(TAG, "address_found");
+
+            deliverResultToReceiver(Constants.SUCCESS_RESULT, address.getSubAdminArea());
+        }
+
+
+    }
+
+    private void deliverResultToReceiver(int resultCode, String message) {
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.RESULT_DATA_KEY, message);
+        mReceiver.send(resultCode, bundle);
+    }
+}
