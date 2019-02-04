a655197e1022799aa0a3adb49e571a202ecc2e62
Aditya-Sood <sood.aditya.08@gmail.com>
2019-02-05 02:57:50 +0530

    Fix news item date display

--- app/src/main/java/com/example/sood/localnews/NewsItem.java
+++ app/src/main/java/com/example/sood/localnews/NewsItem.java
@@ -2,6 +2,11 @@ package com.example.sood.localnews;
 
 import android.graphics.Bitmap;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by sood on 1/30/19.
  */
@@ -10,20 +15,25 @@ public class NewsItem {
 
     private String title;
     private String source;
-    private String date;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+    private Date date;
     private Bitmap icon;
     private String url;
     private String urlToIcon = "https://media1.s-nbcnews.com/j/newscms/2019_05/2734146/190129-cold-weather-ac-929p_a786534c16a83cc04ae303de51bd693a.nbcnews-fp-1200-630.jpg";
 
     public NewsItem(String title, String source, String date, String url, String urlToIcon) {
 
-        this.title = title;
-        this.source = source;
-        this.date = date;
-        this.url = url;
+        if(title != null)   this.title = title;
+        if(source != null)  this.source = source;
+        if(url != null)     this.url = url;
 
-        if(urlToIcon != null && !urlToIcon.isEmpty())
-            this.urlToIcon = urlToIcon;
+        try {
+            this.date = simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            this.date = null;
+        }
+
+        if(urlToIcon != null && !urlToIcon.isEmpty())   this.urlToIcon = urlToIcon;
     }
 
     public String getTitle() {
@@ -35,7 +45,7 @@ public class NewsItem {
     }
 
     public String getDate() {
-        return date;
+        return (SimpleDateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(date));
     }
 
     public Bitmap getIcon() {
