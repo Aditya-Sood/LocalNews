14a22ce7dc6d99a66b501f345bdfb1acbd18d410
Aditya-Sood <sood.aditya.08@gmail.com>
2019-02-05 04:12:40 +0530

    NewsApiUrls - Add 'country' parameter for general news search

--- app/src/main/java/com/example/sood/localnews/NewsApiUrls.java
+++ app/src/main/java/com/example/sood/localnews/NewsApiUrls.java
@@ -8,6 +8,7 @@ public class NewsApiUrls {
 
     private static final String API_KEY = "9a274165beeb47e4a7493e149d6ab586";
     private static final String LANGUAGE = "en";
+    private static final String COUNTRY = "in";
     private static final String PAGE_SIZE = "10";
 
     private static final String PROTOCOL = "https";
@@ -18,14 +19,11 @@ public class NewsApiUrls {
     private static final String ENDPOINT_EVERYTHING = "everything";
 
     private static final String BASE_URL_TOPHEADLINES = PROTOCOL+"://"+HOST+"/"+PATH+"/"+ENDPOINT_TOPHEADLINES+"?apiKey="+API_KEY+"&language="+LANGUAGE+"&pageSize="+PAGE_SIZE;
-    private static final String BASE_URL_EVERYTHING = PROTOCOL+"://"+HOST+"/"+PATH+"/"+ENDPOINT_EVERYTHING+"?apiKey="+API_KEY+"&language="+LANGUAGE+"&pageSize="+PAGE_SIZE+"&q=";
+    private static final String BASE_URL_EVERYTHING = PROTOCOL+"://"+HOST+"/"+PATH+"/"+ENDPOINT_EVERYTHING+"?apiKey="+API_KEY+"&country="+COUNTRY+"&language="+LANGUAGE+"&pageSize="+PAGE_SIZE+"&q=";
 
     public static String getBaseUrlTopheadlines() {
         return  BASE_URL_TOPHEADLINES;
     }
     public static String getBaseUrlEverything(String keyword) { return BASE_URL_EVERYTHING+"\""+keyword+"\"";   }
-
-    //public static final String
-    //public static final String
 
 }
